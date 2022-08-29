6b978e3436691abea50940b547ba574af7bea045
denys1996 <dvchf26@gmail.com>
2022-08-29 10:20:10 -0400

    13.5 Coding Events Setup

---
+++ src/main/java/org/launchcode/codingevent/controllers/EventController.java
@@ -0,0 +1,25 @@
+package org.launchcode.codingevent.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        ArrayList<String> events = new ArrayList<>();
+        events.add("Code With Pride");
+        events.add("Strange Loop");
+        events.add("Apple WWDC");
+        events.add("SpringOne Platform");
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+}
