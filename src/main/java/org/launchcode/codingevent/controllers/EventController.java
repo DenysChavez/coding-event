60e0b4f502965afe42624e244b2f1e39dfce964d
denys1996 <dvchf26@gmail.com>
2022-08-29 12:10:37 -0400

    Add a Form Handler Method

--- src/main/java/org/launchcode/codingevent/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevent/controllers/EventController.java
@@ -3,6 +3,7 @@ package org.launchcode.codingevent.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,13 +13,9 @@ import java.util.ArrayList;
 @RequestMapping("events")
 public class EventController {
 
+    private static ArrayList<String> events = new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model) {
-        ArrayList<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -27,5 +24,12 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm() {
         return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:";
     }
 }
