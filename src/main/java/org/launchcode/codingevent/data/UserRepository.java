46f1442f3913f1f7f7cd5f0454fbc151b4483a66
denys1996 <dvchf26@gmail.com>
2022-10-08 11:28:05 -0400

    19.4 and 19.5 book

---
+++ src/main/java/org/launchcode/codingevent/data/UserRepository.java
@@ -0,0 +1,8 @@
+package org.launchcode.codingevent.data;
+import org.launchcode.codingevent.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
